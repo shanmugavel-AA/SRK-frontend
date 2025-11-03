20605405eb46e770ebf25a19111092094aad420e
unknown <ashanmugavel.csbs2021@citchennai.net>
2025-11-03 10:20:57 +0530

    pusing next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@
 const nextConfig = {
     productionBrowserSourceMaps: false,
     images: {
-    // Add all external image domains you use (like Cloudinary or your API)
     domains: ['res.cloudinary.com', 'api.sharathravikumar.com'],
   },
 };
